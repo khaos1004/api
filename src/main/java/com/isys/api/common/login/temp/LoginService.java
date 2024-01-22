ad7dc86ba6a6e380aa303add2bab763c99e3350e
김의서 <kes@2isys.com>
2024-01-22 17:59:07 +0900

    jwt

--- src/main/java/com/isys/api/common/login/temp/LoginService.java
+++ src/main/java/com/isys/api/common/login/temp/LoginService.java
@@ -2,21 +2,18 @@ package com.isys.api.common.login.temp;
 
 import com.isys.api.entity.UserInfoEntity;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.User;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class LoginService {
     private LoginRepository loginRepository;
 
-    public UserInfoEntity auth(LoginRequestDTO loginRequestDTO){
+    public String auth(LoginRequestDTO loginRequestDTO){
         UserInfoEntity findUser = loginRepository.findByName(loginRequestDTO.getName());
         if (findUser != null){
-            LoginRequestDTO result = loginRequestDTO.
+
         }
-            return findUser;
+            return "";
     }
 }
